ddddae971ef6c8a646df446e8b40d758577afe2a
Inal_jj <7unusbekkovv@gmail.com>
2025-05-03 14:12:12 +0600

    commit2

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import FirstSection from "../pages/firstSection/FirstSection";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import Categories from "../pages/secondsection/Categories"
+import ThirdSection from "../pages/thirdsection/ThirdSection"
 import scss from "./Layout.module.scss";
 
 
@@ -12,6 +13,7 @@ const Layout = () => {
         <main>
           <FirstSection/>
           <Categories/>
+          <ThirdSection/>
         </main>
         <Footer />
     </div>
