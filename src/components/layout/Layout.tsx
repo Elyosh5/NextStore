dcb173f6d7961822d289c2893f0d75edbcc15126
Inal_jj <7unusbekkovv@gmail.com>
2025-05-03 13:15:29 +0600

    commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,15 @@
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import Categories from "../pages/secondsection/Categories"
 import scss from "./Layout.module.scss";
+
 
 const Layout = () => {
   return (
     <div className={scss.Layout}>
       <div className="container">
         <Header />
-        <main></main>
+        <main><Categories></Categories></main>
         <Footer />
       </div>
     </div>
---
+++ src/components/pages/secondsection/Categories.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './Categories.scss';
+
+const Categories: React.FC = () => {
+  return (
+    <section className="categories-section">
+      <h2>Browse Categories</h2>
+      <p>Find exactly what you need from our wide range of electronic categories.</p>
+      <div className="categories">
+        <div className="category-item">
+
+					
+				</div>
+      </div>
+    </section>
+  );
+};
+
+export default Categories;
